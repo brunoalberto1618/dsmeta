eb85119f66819bdd959c598e1ef6cf1748d7f282
brunoalberto1618 <brunoalberto1618@gmail.com>
2023-01-19 20:27:09 -0300

    Other components

---
+++ frontend/src/assets/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from "../../img/logo.svg"
+
+import "./styles.css"
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por
+                        <a target="_blank" href="https://github.com/brunoalberto1618">github.com/brunoalberto1618</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
