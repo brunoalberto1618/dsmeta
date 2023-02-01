e8e2b51f861bfe0ec27a2006167981a7a6ba1489
brunoalberto1618 <brunoalberto1618@gmail.com>
2023-02-01 16:24:52 -0300

    Header Update

--- frontend/src/assets/components/Header/index.tsx
+++ frontend/src/assets/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                     <h1>DSMeta</h1>
                     <p>
                         Desenvolvido por
-                        <a target="_blank" href="https://github.com/brunoalberto1618">github.com/brunoalberto1618</a>
+                        <a target="_blank" href="https://github.com/brunoalberto1618"> github.com/brunoalberto1618</a>
                     </p>
             </div>
         </header>
